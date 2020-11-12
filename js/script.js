da842ff331af6cf5b0085513fd2de910a63b0f63
mouhamed bakhoum <bakhoumouhamed@gmail.com>
2020-11-12 16:07:45 +0000

    Finished responsive main menu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
+/*
+Fonctions pour le responsive
+*/
+
 setDropdownPosition = () => {
     document.getElementById('u-info').style.right = ($(window).width() - document.getElementById('connect-btn').offsetLeft - $('#connect-btn').width()*1.3)+'px'
+}
+unsetDropdownPosition = () => {
+    document.getElementById('u-info').style.right = 'initial'
 }
 setNavMenuPosition = () => {
     document.getElementById("nav-menu").style.top = document.getElementById('navbar').offsetHeight+"px"
 }
 enableResponsiveMenu = () => {
+    unsetDropdownPosition()
     menu = document.getElementById('category-list')
     nav = document.getElementById('nav-menu')
     nav.innerHTML += menu.innerHTML
@@ -14,18 +22,24 @@ disableResponsiveMenu = () => {
     $('#nav-menu ul#Categories').remove()
     menu = document.getElementById('category-list')
     menu.style.display = "initial"
+    setDropdownPosition()
 }
+
+/*
+Au chargement du document
+*/
+
 $(function () {
+    setNavMenuPosition()
+
     if ($(window).width()<769) {
         enableResponsiveMenu()
     }
-    setNavMenuPosition()
     $(window).on('resize', () => {
         if ($(window).width()<769) {
             enableResponsiveMenu()
         }
         else{
-            setDropdownPosition()
             disableResponsiveMenu()
         }
     })
